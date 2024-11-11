71fa0e809d5ee51ecfb24d1291cc2850ab64af12
TsengMingYi <www.mingerzeng@gmail.com>
2024-11-11 15:48:36 +0800

    上傳檔案

---
+++ app/http/app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time   : 2024/8/11 2:14 上午
+@Author : www.mingerzeng@gmail.com
+@File : app.py
+"""
+from flask_cors import CORS
+from flask_migrate import Migrate
+from injector import Injector
+from internal.router import Router
+from internal.server.http import Http
+import dotenv
+from config import Config
+from .module import ExtensionModule
+from pkg.sqlalchemy import SQLAlchemy
+
+# 將env加載到環境變量中
+dotenv.load_dotenv()
+
+conf = Config()
+
+
+injector = Injector([ExtensionModule])
+
+app = Http(__name__, conf=conf, db=injector.get(SQLAlchemy),migrate=injector.get(Migrate),router=injector.get(Router))
+# CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
+
+celery = app.extensions["celery"]
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
---
+++ app/http/module.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time   : 2024/8/13 下午1:42
+@Author : www.mingerzeng@gmail.com
+@File : module.py
+"""
+from pkg.sqlalchemy import SQLAlchemy
+from injector import Module, Binder
+from flask_migrate import Migrate
+from internal.extension.migrate_extension import migrate
+from internal.extension.redis_extension import redis_client
+from internal.extension.database_extension import db
+from redis import Redis
+
+
+class ExtensionModule(Module):
+    """擴展模塊的依賴注入"""
+
+    def configure(self, binder: Binder) -> None:
+        binder.bind(SQLAlchemy,to=db)
+        binder.bind(Migrate,to=migrate)
+        binder.bind(Redis, to=redis_client)
